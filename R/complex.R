b0441723093406936c71d2251a1dc90a1c6bd16c
Duccio Rocchini <ducciorocchini@gmail.com>
2024-12-13 11:34:03 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -1,7 +1,13 @@
 complex <- function(x){
 if(x>0){
   print("Positive number")
-}
-else
+  }
+else if(x=0)
+  {
+  print("Hey, don't miss around. Zero is nor negative nor positive okkkkk?????")
+  }
+else if(x<0)
+  {
   print("Negative number")
   }
+}
